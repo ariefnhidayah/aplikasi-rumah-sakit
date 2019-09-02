69fc3ce0fb668a55c55761c7cf186cb73439fa2c
Arief Nur Hidayah <48835089+ariefnhidayah@users.noreply.github.com>
2019-09-02 19:30:30 +0700

    Update header.php

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['admin'])) {
 	<title>Aplikasi - Rumah Sakit</title>
 
 	<!-- Bootstrap core CSS -->
-	<link href="<?= base_url(); ?>/_assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 	<!-- Custom styles for this template -->
 	<link href="<?= base_url('_assets/css/simple-sidebar.css') ?>" rel="stylesheet">
